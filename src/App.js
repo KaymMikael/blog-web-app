bc13372880f4afe516b60f09d46de87baefe2a4c
Sero <kmcaltiz@gmail.com>
2024-09-30 07:50:28 +0800

    add the notfound component to routes and add google fonts to index.html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./home/Home";
 import Login from "./login/Login";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/login"} element={<Login />} />
+          <Route path={"*"} element={<NotFound />} />
         </Routes>
       </main>
     </div>
