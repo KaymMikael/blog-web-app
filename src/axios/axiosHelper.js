d914c60cf62e9bc6c0edee48562036d73ba78686
Sero <kmcaltiz@gmail.com>
2024-10-10 15:23:02 +0800

    change base url to render.com

--- src/axios/axiosHelper.js
+++ src/axios/axiosHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosHelper = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://blog-web-app-api.onrender.com",
 });
 
 export default axiosHelper;
